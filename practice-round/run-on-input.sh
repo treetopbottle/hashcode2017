e7a7b7c5e2c713d50198b0adec4fa61d2ea326e2
anonimized <anonimized>
2017-02-21 21:53:41 +0100

    add bash script to run on input

---
+++ practice-round/run-on-input.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+./solution.py input-sets/big.in > output/big.out
+./solution.py input-sets/example.in > output/example.out
+./solution.py input-sets/medium.in > output/medium.out
+./solution.py input-sets/small.in > output/small.out
