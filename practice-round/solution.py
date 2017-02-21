4296daf8d95bbc982a29f5945501336c2b573663
anonimized <anonimized>
2017-02-21 21:17:29 +0100

    improve slices output

--- practice-round/solution.py
+++ practice-round/solution.py
@@ -9,7 +9,17 @@ rows, columns, min_ingredients, max_slices = next(lines).split(' ')
 for line in lines:
     pass
 
-slices = []
+slices = [[0,0,1,1]]
 
+
+# Output
+
+# The total number of slices to be cut
 print(len(slices))
-print('\n'.join(slices))
+
+# The slices
+string_slices = []
+for slice_ in slices:
+    string_slice = ' '.join([str(s) for s in slice_])
+    string_slices.append(string_slice)
+print('\n'.join(string_slices))
