6f20685162848babbb9b456bb3da6fc365084534
anonimized <anonimized>
2017-02-23 21:20:19 +0100

    determine latency improvement per cache

--- assignment/solution.py
+++ assignment/solution.py
@@ -26,7 +26,8 @@ request_descriptions = []
 for i in range(nr_request_descriptions):
     video_id, endpoint_id, nr_requests = [int(i) for i in lines.pop(0).split(' ')]
     request_descriptions.append((video_id, endpoint_id, nr_requests))
-    endpoint_to_request_description[endpoint_id].append((video_id, nr_requests))
+    latency = endpoints[endpoint_id][0]
+    endpoint_to_request_description[endpoint_id].append((video_id, nr_requests, latency))
 
 # Transformation
 cache_to_endpoint = collections.defaultdict(list)
@@ -34,15 +35,15 @@ for endpoint_nr, endpoint in enumerate(endpoints):
     (latency, connections) = endpoint
     for connection in connections:
         (cache_nr, latency_endpoint_to_cache) = connection
-        cache_to_endpoint[cache_nr].append((endpoint_nr,
-                                            latency - latency_endpoint_to_cache))
+        cache_to_endpoint[cache_nr].append((endpoint_nr, latency_endpoint_to_cache))
 
 # Generate Solution
 def score(cache):
     video_to_score = collections.defaultdict(int)
-    for endpoint_nr, latency_improvements in cache_to_endpoint[cache]:
-        for video_id, nr_requests in endpoint_to_request_description[endpoint_nr]:
-            video_to_score[video_id] += nr_requests * latency_improvements
+    for endpoint_nr, endpoint_latency in cache_to_endpoint[cache]:
+        for video_id, nr_requests, current_latency in endpoint_to_request_description[endpoint_nr]:
+            video_to_score[video_id] += nr_requests \
+                                        * max(0, current_latency - endpoint_latency)
     return video_to_score
 
 def cache_to_candidates(cache_id):
@@ -58,9 +59,18 @@ def videos_for_cache(cache_id):
             remaining_size -= videos_mb[candidate]
             yield candidate
 
+def update_request_description(cache_nr, videos):
+    for endpoint,latency_for_cache in cache_to_endpoint[cache_nr]:
+        for i,description in enumerate(endpoint_to_request_description[endpoint]):
+            (video_id, nr_requests, current_latency) = description
+            if video_id in videos:
+                endpoint_to_request_description[endpoint][i] = (video_id, nr_requests, min(current_latency, latency_for_cache))
+
 cache_server_descriptions = {}
 for cache_nr in range(nr_cache_servers):
-    cache_server_descriptions[cache_nr] = list(videos_for_cache(cache_nr))
+    videos_to_place_in_cache = list(videos_for_cache(cache_nr))
+    cache_server_descriptions[cache_nr] = videos_to_place_in_cache
+    update_request_description(cache_nr, cache_server_descriptions[cache_nr])
 
 print(len(cache_server_descriptions))
 for i in cache_server_descriptions:
