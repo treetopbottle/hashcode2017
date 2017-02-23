b9fcd39d51414974a0a6ed648ed1a2eea7f9b994
anonimized <anonimized>
2017-02-23 19:06:26 +0100

    read input videos and endpoints

--- assignment/solution.py
+++ assignment/solution.py
@@ -6,10 +6,28 @@ import sys
 (header, *lines) = fileinput.input()
 lines = [l.strip() for l in lines]
 
-_, _ = \
+nr_videos, nr_endpoints, nr_request_descriptions, nr_cache_servers, capacity =\
     [int(i) for i in header.strip().split(' ')]
 
+videos_mb = [int(i) for i in lines.pop(0).split(' ')]
+print(videos_mb)
 
+endpoints = []
+for i in range(nr_endpoints):
+    latency, nr_connected_cache_servers = [int(i) for i in lines.pop(0).split(' ')]
+    connected_cache_servers = []
+    for j in range(nr_connected_cache_servers):
+        next_line = lines.pop(0)
+        id_, latency = [int(i) for i in next_line.split(' ')]
+        connected_cache_servers.append((id_, latency))
+    endpoints.append((latency, connected_cache_servers))
+print(endpoints[0])
+
+for i in range(nr_request_descriptions):
+    pass
 
 # Output
+
+
+print('---')
 
