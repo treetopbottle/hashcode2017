9d0db50335f0d6d8eeaa5108772f4bbf80b7f3f5
anonimized <anonimized>
2017-02-23 19:13:01 +0100

    make run-on-input give progress info

--- assignment/run-on-input.sh
+++ assignment/run-on-input.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
-./solution.py input-sets/kittens.in > output/kittens.out
+echo "me_at_the_zoo"
 ./solution.py input-sets/me_at_the_zoo.in > output/me_at_the_zoo.out
-./solution.py input-sets/trending_today.in > output/trending_today.out
+echo "done"
+echo
+
+echo "videos_worth_spreading"
 ./solution.py input-sets/videos_worth_spreading.in > output/videos_worth_spreading.out
+echo "done"
+echo
+
+echo "trending_today"
+./solution.py input-sets/trending_today.in > output/trending_today.out
+echo "done"
+echo
+
+echo "kittens.in"
+./solution.py input-sets/kittens.in > output/kittens.out
+echo "done"
