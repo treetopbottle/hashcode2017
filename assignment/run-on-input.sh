01b479e248f2e94f120e7b3a8644357ed6cac7a9
anonimized <anonimized>
2017-02-23 19:06:41 +0100

    update run-on-input.sh for assignment

--- assignment/run-on-input.sh
+++ assignment/run-on-input.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-./solution.py input-sets/.in > output/.out
-./solution.py input-sets/.in > output/.out
-./solution.py input-sets/.in > output/.out
-./solution.py input-sets/.in > output/.out
+./solution.py input-sets/kittens.in > output/kittens.out
+./solution.py input-sets/me_at_the_zoo.in > output/me_at_the_zoo.out
+./solution.py input-sets/trending_today.in > output/trending_today.out
+./solution.py input-sets/videos_worth_spreading.in > output/videos_worth_spreading.out
